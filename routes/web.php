1b635a6a84c16952ac3316a58631e0827aff581c
fateme mozafari <fatemem1990@yahoo.com>
2022-09-06 12:22:24 +0430

    edit web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\AboutController;
+use App\Http\Controllers\admin\ContactController;
+use App\Http\Controllers\admin\CourseController;
+use App\Http\Controllers\admin\DocumentController;
+use App\Http\Controllers\admin\GalleryController;
+use App\Http\Controllers\admin\NewsController;
+use App\Http\Controllers\admin\OnlineExamController;
+use App\Http\Controllers\admin\QuestionController;
+use App\Http\Controllers\admin\ResultController;
+use App\Http\Controllers\admin\TeacherController;
+use App\Http\Controllers\admin\UserController;
+use \App\Http\Controllers\auth\AuthController;
+use \App\Http\Controllers\auth\VerifyEmailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,18 +29,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
+
+
 /*
 |--------------------------------------------------------------------------
 | auth Routes
 |--------------------------------------------------------------------------
 |
 */
-//Route::middleware('auth')->group(function () {
-    Route::view('login', 'auth.login')->name('login');
-    Route::view('register', 'auth.register')->name('register');
-    Route::view('forgetPassword', 'auth.forgetPassword')->name('forgetpassword');
-    Route::view('resetPassword', 'auth.resetPassword')->name('resetpassword');
-//});
+
 /*
 |--------------------------------------------------------------------------
 | admin Routes
@@ -36,72 +46,77 @@ Route::get('/', function () {
 */
 Route::prefix('admin')->group(function (){
     Route::view('dashboard','admin.dashboard')->name('dashboard');
-    Route::resource('about',\App\Http\Controllers\admin\AboutController::class)->names([
+
+    Route::resource('about',AboutController::class)->names([
         'create'=>'about.create',
         'edit'=>'about.edit',
         'show'=>'about.show',
         'index'=>'about.list',
     ]);
-    Route::resource('contact',\App\Http\Controllers\admin\ContactController::class)->names([
+    Route::resource('contact',ContactController::class)->names([
         'create'=>'contact.create',
         'edit'=>'contact.edit',
         'show'=>'contact.show',
         'index'=>'contact.list',
     ]);
-    Route::resource('courses',\App\Http\Controllers\admin\CourseController::class)->names([
+    Route::resource('courses',CourseController::class)->names([
         'create'=>'course.create',
         'edit'=>'course.edit',
         'show'=>'course.show',
         'index'=>'course.list',
     ]);
-    Route::resource('documents',\App\Http\Controllers\admin\DocumentController::class)->names([
+    Route::resource('documents',DocumentController::class)->names([
         'create'=>'document.create',
         'edit'=>'document.edit',
         'show'=>'document.show',
         'index'=>'document.list',
     ]);
-    Route::resource('gallery',\App\Http\Controllers\admin\GalleryController::class)->names([
+    Route::resource('gallery',GalleryController::class)->names([
         'create'=>'gallery.create',
         'edit'=>'gallery.edit',
         'show'=>'gallery.show',
         'index'=>'gallery.list',
     ]);
-    Route::resource('news',\App\Http\Controllers\admin\NewsController::class)->names([
+    Route::resource('news',NewsController::class)->names([
         'create'=>'news.create',
         'edit'=>'news.edit',
         'show'=>'news.show',
         'index'=>'news.list',
     ]);
-    Route::resource('online_exams',\App\Http\Controllers\admin\OnlineExamController::class)->names([
+    Route::resource('online_exams',OnlineExamController::class)->names([
         'create'=>'online_exam.create',
         'edit'=>'online_exam.edit',
         'show'=>'online_exam.show',
         'index'=>'online_exam.list',
     ]);
-    Route::resource('questions',\App\Http\Controllers\admin\QuestionController::class)->names([
+    Route::resource('questions',QuestionController::class)->names([
         'create'=>'question.create',
         'edit'=>'question.edit',
         'show'=>'question.show',
         'index'=>'question.list',
     ]);
-    Route::resource('results',\App\Http\Controllers\admin\ResultController::class)->names([
+    Route::resource('results',ResultController::class)->names([
         'create'=>'result.create',
         'edit'=>'result.edit',
         'show'=>'result.show',
         'index'=>'result.list',
     ]);
-    Route::resource('teachers',\App\Http\Controllers\admin\TeacherController::class)->names([
+    Route::resource('teachers',TeacherController::class)->names([
         'create'=>'teacher.create',
         'edit'=>'teacher.edit',
         'show'=>'teacher.show',
         'index'=>'teacher.list',
     ]);
-    Route::resource('users',\App\Http\Controllers\admin\UserController::class)->names([
+    Route::resource('users',UserController::class)->names([
         'create'=>'user.create',
         'edit'=>'user.edit',
         'show'=>'user.show',
         'index'=>'user.list',
     ]);
+    Route::view('event','admin.courses.event')->name('event');
+    Route::view('event2','admin.teachers.event')->name('teacher.event');
+    Route::view('event3','admin.users.event')->name('user.event');
+
 });
 /*
 |--------------------------------------------------------------------------
