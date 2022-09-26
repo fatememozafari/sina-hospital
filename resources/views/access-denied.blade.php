999da4415845fdb0893ffd602d2cfc347388c93a
fateme mozafari <fatemem1990@yahoo.com>
2022-09-26 12:32:13 +0330

    add access-denied.blade.php

---
+++ resources/views/access-denied.blade.php
@@ -0,0 +1,51 @@
+@extends('auth.layout.masterPage')
+@section('content')
+    <div class="container">
+        <div class="col-md-12 content-center">
+            <div class="card-plain">
+                <div class="header">
+                    <div class="logo-container">
+                        <img src="assets/images/logo.svg" alt="">
+                    </div>
+                    <h2>-403-</h2>
+{{--                    <h3>access denied</h3>--}}
+                    <h3>دسترسی شما محدود شده است</h3>
+                </div>
+{{--                <form action="{{route('login')}}" method="post">--}}
+{{--                    @csrf--}}
+{{--                    <div class="content">--}}
+{{--                        <div class="input-group input-lg">--}}
+{{--                            <input type="text" name="username" class="form-control" placeholder="ایمیل / موبایل ">--}}
+{{--                            <span class="input-group-addon">--}}
+{{--                                <i class="zmdi zmdi-account-circle"></i>--}}
+{{--                            </span>--}}
+{{--                        </div>--}}
+{{--                        @error('username')--}}
+{{--                        <span class=" alert-danger" role="alert">--}}
+{{--                                <strong>{{$message}}</strong>--}}
+{{--                            </span>--}}
+{{--                        @enderror--}}
+{{--                        <div class="input-group input-lg">--}}
+{{--                            <input type="password" name="password" placeholder="رمز عبور" class="form-control" />--}}
+{{--                            <span class="input-group-addon">--}}
+{{--                                <i class="zmdi zmdi-lock"></i>--}}
+{{--                            </span>--}}
+{{--                        </div>--}}
+{{--                        @error('password')--}}
+{{--                        <span class=" alert-danger" role="alert">--}}
+{{--                                <strong>{{$message}}</strong>--}}
+{{--                            </span>--}}
+{{--                        @enderror--}}
+{{--                    </div>--}}
+{{--                    <div class="footer text-center">--}}
+{{--                        <button type="submit" class="btn l-cyan btn-round btn-lg btn-block waves-effect waves-light">ورود</button>--}}
+{{--                    </div>--}}
+{{--                </form>--}}
+                <div class="footer text-center">
+                    <h6 class="m-t-20"><a href="/forgot-password" class="link">رمز عبورتان را فراموش کرده اید ؟</a></h6>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
