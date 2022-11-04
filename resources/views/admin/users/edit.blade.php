3cce68575731acee8f706c8c31e5c26bdbf69664
fatememozafari <95769746+fatememozafari@users.noreply.github.com>
2022-11-04 10:55:25 +0330

    add validation and remove li in admin/users/edit.blade.php

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -4,7 +4,7 @@
         <div class="block-header">
             <div class="row">
                 <div class="col-lg-7 col-md-6 col-sm-12">
-                    <h2> ویرایش کاربر{{$inputs->name}}
+                    <h2> ویرایش کاربر {{$inputs->name}} {{$inputs->family}}
                         <small class="text-muted">خوش امدید</small>
                     </h2>
                 </div>
@@ -21,18 +21,11 @@
         </div>
         <div class="container-fluid">
             <div class="row clearfix">
-                <div class="col-lg-12 col-md-12 col-sm-12">
+                <div class="col-lg-9 col-md-9 col-sm-9">
                     <div class="card">
                         <div class="header">
-                            <h2> اطلاعات <strong>پایه</strong> <small>متن توضیحات در اینجا ...</small> </h2>
+                            <h2>  <strong></strong> <small></small> </h2>
                             <ul class="header-dropdown">
-                                <li class="dropdown"> <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i class="zmdi zmdi-more"></i> </a>
-                                    <ul class="dropdown-menu dropdown-menu-right slideUp float-right">
-                                        <li><a href="javascript:void(0);">ویرایش</a></li>
-                                        <li><a href="javascript:void(0);">حذف</a></li>
-                                        <li><a href="javascript:void(0);">گزارش</a></li>
-                                    </ul>
-                                </li>
                                 <li class="remove">
                                     <a role="button" class="boxs-close"><i class="zmdi zmdi-close"></i></a>
                                 </li>
@@ -45,7 +38,10 @@
                                 <div class="row clearfix">
                                     <div class="col-sm-12"><b>نام</b>
                                         <div class="form-group">
-                                            <input type="text" name="name" value="{{$inputs->name}}" class="form-control" placeholder="نام">
+                                            <input type="text" name="name" value="{{$inputs->name}}" class="form-control" placeholder="نام"
+                                                   @error('name')
+                                                   style="border: 1px solid red"
+                                                @enderror>
                                         </div>
                                     </div>
                                 </div>
@@ -53,21 +49,30 @@
 
                                     <div class="col-sm-12"><b>نام خانوادگی</b>
                                         <div class="form-group">
-                                            <input type="text" name="family" value="{{$inputs->family}}" class="form-control" placeholder="نام خانوادگی">
+                                            <input type="text" name="family" value="{{$inputs->family}}" class="form-control" placeholder="نام خانوادگی"
+                                                   @error('family')
+                                                   style="border: 1px solid red"
+                                                @enderror>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row clearfix">
                                     <div class="col-sm-12"><b>کدملی</b>
                                         <div class="form-group">
-                                            <input type="text" name="melli_code" value="{{$inputs->melli_code}}" class="form-control" placeholder="کد ملی">
+                                            <input type="text" name="melli_code" value="{{$inputs->melli_code}}" class="form-control" placeholder="کد ملی"
+                                                   @error('melli_code')
+                                                   style="border: 1px solid red"
+                                                @enderror>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row clearfix">
                                     <div class="col-sm-12">
                                         <div class="form-group">
-                                            <select class="form-control show-tick" name="gender">
+                                            <select class="form-control show-tick" name="gender"
+                                                    @error('gender')
+                                                    style="border: 1px solid red"
+                                                @enderror>
                                                 <option value="">- جنسیت -</option>
                                                 <option value="FEMALE">خانم</option>
                                                 <option value="MALE">آقا</option>
@@ -75,75 +80,87 @@
                                         </div>
                                     </div>
                                 </div>
-                                {{--                                <div class="row clearfix">--}}
-                                {{--                                    <div class="form-group">--}}
-                                {{--                                        <div class="radio inlineblock m-l-20">--}}
-                                {{--                                            <input type="radio" name="gender" id="male" class="with-gap" value="option1">--}}
-                                {{--                                            <label for="MALE">آقا</label>--}}
-                                {{--                                        </div>--}}
-                                {{--                                        <div class="radio inlineblock">--}}
-                                {{--                                            <input type="radio" name="gender" id="Female" class="with-gap" value="option2" checked="">--}}
-                                {{--                                            <label for="FEMAIL">خانم</label>--}}
-                                {{--                                        </div>--}}
-                                {{--                                    </div>--}}
-                                {{--                                </div>--}}
-
                                 <div class="row clearfix">
                                     <div class="col-sm-12"><b>شماره موبایل</b>
                                         <div class="form-group">
-                                            <input type="text" name="mobile"  value="{{$inputs->mobile}}" class="form-control" placeholder="موبایل">
+                                            <input type="text" name="mobile"  value="{{$inputs->mobile}}" class="form-control" placeholder="موبایل"
+                                                   @error('mobile')
+                                                   style="border: 1px solid red"
+                                                @enderror>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row clearfix">
                                     <div class="col-sm-12"><b>ایمیل</b>
                                         <div class="form-group">
-                                            <input type="text" name="email" value="{{$inputs->email}}" class="form-control" placeholder="ایمیل">
+                                            <input type="text" name="email" value="{{$inputs->email}}" class="form-control" placeholder="ایمیل"
+                                                   @error('email')
+                                                   style="border: 1px solid red"
+                                                @enderror>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row clearfix">
-                                    <div class="col-sm-12"><b>رمز ورود</b>
+                                    <div class="col-sm-12">
                                         <div class="form-group">
-                                            <input type="password" name="password" value="{{$inputs->password}}" class="form-control" placeholder="رمز ورود">
+                                            <input type="hidden" name="password" value="{{$inputs->password}}" class="form-control" placeholder="رمز ورود"
+                                                   @error('password')
+                                                   style="border: 1px solid red"
+                                                @enderror>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row clearfix">
-                                    <div class="col-sm-12"><b>تایید رمز ورود</b>
+                                    <div class="col-sm-12"><b> رمز ورود</b>
                                         <div class="form-group">
-                                            <input type="text" name="password_confirmation" value="{{$inputs->password_confirmation}}" class="form-control" placeholder="تایید رمز ورود">
+                                            <input type="text" name="password_confirmation" value="{{$inputs->password_confirmation}}" class="form-control" placeholder="تایید رمز ورود"
+                                                   readonly
+                                                   @error('password_confirmation')
+                                                   style="border: 1px solid red"
+                                                @enderror>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row clearfix">
-                                    <div class="col-sm-12"><b>شغل</b>
+                                    <div class="col-sm-12"><b>تخصص</b>
                                         <div class="form-group">
-                                            <input type="text" name="job" value="{{$inputs->job}}" class="form-control" placeholder="شغل">
+                                            <input type="text" name="job" value="{{$inputs->job}}" class="form-control" placeholder="شغل"
+                                                   @error('job')
+                                                   style="border: 1px solid red"
+                                                @enderror>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row clearfix">
                                     <div class="col-sm-12"><b>تاریخ تولد</b>
                                         <div class="form-group">
-                                            <input type="date" name="birthday" value="{{$inputs->birthday}}" class="form-control" placeholder="تاریخ تولد">
+                                            <input type="date" name="birthday" value="{{$inputs->birthday}}" class="form-control" placeholder="تاریخ تولد"
+                                                   @error('birthday')
+                                                   style="border: 1px solid red"
+                                                @enderror>
                                         </div>
                                     </div>
                                 </div>
-
                                 <div class="col-lg-12 col-md-12 col-sm-12"><b>آپلود تصویر</b>
                                     <div class="dz-message">
                                         <div class="drag-icon-cph"> <i class="material-icons">touch_app</i> </div>
-                                        <h3> تصویر را در اینجا رها کنید یا برای بارگذاری کلیک کنید. </h3>
+
                                         <em> <strong></strong></em> </div>
                                     <div class="fallback">
-                                        <input name="avatar_path" type="file" value="{{$inputs->avatar_path}}" multiple />
+                                        <input name="avatar_path" type="file" value="{{$inputs->avatar_path}}" multiple
+                                               @error('avatar_path')
+                                               style="border: 1px solid red"
+                                            @enderror>
                                     </div>
                                 </div>
+                                <br>
                                 <div class="row clearfix">
                                     <div class="col-sm-12"><b>آدرس</b>
                                         <div class="form-group">
-                                            <textarea class="form-control" name="address" value="" placeholder="آدرس">{{$inputs->address}}</textarea>
+                                            <textarea class="form-control" name="address" value="" placeholder="آدرس"
+                                                      @error('address')
+                                                      style="border: 1px solid red"
+                                            @enderror>{{$inputs->address}}</textarea>
                                         </div>
                                     </div>
                                 </div>
@@ -153,6 +170,16 @@
                             </form>
                         </div>
                     </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3">
+                    <p>images:</p>
+                        <form action="/admin/deletephoto/{{$inputs->id}}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button class="btn text-danger text-center" type="submit">X</button>
+                        </form>
+                        <img src="{{$inputs->avatar_path}}" class="img-responsive"
+                             style="max-width: 200px; max-height: 200px" alt="">
                 </div>
             </div>
         </div>
