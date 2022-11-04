e7d909bf0a7e49cd94e903194d42329bed18e797
fatememozafari <95769746+fatememozafari@users.noreply.github.com>
2022-11-04 10:14:21 +0330

    add user_id to contacts table

--- database/migrations/2022_08_29_055144_create_contacts_table.php
+++ database/migrations/2022_08_29_055144_create_contacts_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('message');
             $table->string('file');
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
             $table->softDeletes();
 
