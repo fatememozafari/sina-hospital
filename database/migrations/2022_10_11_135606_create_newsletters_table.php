d261582f4f5f58b9ed4a05453df09ceee0ee78a4
fatememozafari <95769746+fatememozafari@users.noreply.github.com>
2022-12-07 12:17:26 +0330

    make some fields of table nullable

--- database/migrations/2022_10_11_135606_create_newsletters_table.php
+++ database/migrations/2022_10_11_135606_create_newsletters_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('newsletters', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email');
             $table->foreignId('user_id');
             $table->timestamps();
