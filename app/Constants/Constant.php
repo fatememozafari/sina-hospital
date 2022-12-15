86e352dcc32cde3b9af582dff1beab4b5d7431be
fatememozafari <95769746+fatememozafari@users.noreply.github.com>
2022-12-15 15:08:54 +0330

    add Constant

---
+++ app/Constants/Constant.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Constants;
+
+class Constant
+{
+    const CONTACT_FILE_UPLOAD_PATH = 'uploads'.DIRECTORY_SEPARATOR.'contacts'.DIRECTORY_SEPARATOR.'files'.DIRECTORY_SEPARATOR;
+}
