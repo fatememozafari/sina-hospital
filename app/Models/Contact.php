54e9523b240e7e1fb0fdedb1db17f83b59e761dc
fatememozafari <95769746+fatememozafari@users.noreply.github.com>
2022-12-15 15:12:08 +0330

    add presentable and relation to Contact model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -3,13 +3,23 @@
 namespace App\Models;
 
 use App\Filters\contracts\Filterable;
+use App\Presenters\ContactPresenter;
+use App\presenters\contracts\Presentable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    use HasFactory , Filterable;
+    use HasFactory , Filterable , Presentable;
+    public $presenter = ContactPresenter::class;
+
     protected $fillable=[
-        'name','title','message','file','user_id'
+        'name','title','message','user_id'
     ];
+
+    public function files()
+    {
+        return $this->morphMany(File::class, 'fileable');
+    }
+
 }
