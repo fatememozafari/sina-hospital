ee1c0c299078c5727f8fc175fc507312fabda1e5
fatememozafari <fatemem1990@yahoo.com>
2023-07-15 08:50:55 +0330

    tables related to product sending and delivery

---
+++ database/migrations/2023_07_15_051438_create_deliveries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); //روش ارسال
+            $table->decimal('amount', 20, 3)->nullable(); // هزینه ارسال
+            $table->integer('delivery_time')->nullable();
+            $table->string('delivery_time_unit')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deliveries');
+    }
+};
